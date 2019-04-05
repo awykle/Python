34a3ef69ca43e11d0f1da0a6d2e79cf9467b29a5
awykle <allisonwykle@gmail.com>
2019-04-04 23:48:35 -0500

    Chapter 3, pgs 13-26

--- 1PythonShell_Practice04042019/_1PythonShell_Practice04042019.py
+++ 1PythonShell_Practice04042019/_1PythonShell_Practice04042019.py
@@ -1,1 +1,114 @@
+# http://tinyurl.com/h4qntgk
+# Cory Althoff - the self-taught Programmer guide
+# Chapter 3 - awykle - Practice 2019-04-04
+#-------------------------------------------
+ 
 
+#-------------------------------------------
+# EXAMPLE: pg13
+#-------------------------------------------
+
+for i in range(100):
+    print("Hello, World!")
+
+        # TIP---> Indents must be four spaces
+        #       > Use three quotes if you have a long line of code extending to another line
+        #       > Use a \ to extend code to the next line
+
+#-------------------------------------------
+# EXAMPLE: pg16-17
+#-------------------------------------------
+
+print\
+    ("""This is a really really
+    really really "really long line of
+    code.""")
+
+        # TIP---> You can use single or double quotes for strings as long as they match 
+        #       > Object with the data type NoneType always have the value None. They are used to represent the absence of value. 
+
+#-------------------------------------------
+# EXAMPLE: pg 20
+#-------------------------------------------
+
+print(2+2)
+
+print (2-2)
+
+print (4/2)
+
+print(2*2)
+
+
+        # TIP---> Create a floating number when dividing ints 
+        #       > A constant is a value that never changes (such as the number 2 in the example above)
+        #       > A variable is assigned using the assignment operator (the = sign)
+
+#-------------------------------------------
+# EXAMPLE: pg 21-22
+#-------------------------------------------
+
+b = 100
+print(b)
+
+x = 100
+print(x + b)
+
+x = 200
+print(x + b)
+
+#-------------------------------------------
+# EXAMPLE: pg 23
+#-------------------------------------------
+
+        # TIP---> You may incriment and dedecrement a variable in two ways, below
+
+x = 10
+x = x - 1
+print(x)
+
+x = 10
+x -= 1
+print(x)    
+
+x = 10
+x += 1
+print(x)
+
+#-------------------------------------------
+# EXAMPLE: pg 23-24
+#-------------------------------------------
+
+hi = "Hello, World!"
+print(hi) 
+
+my_float = 2.2
+print(my_float)
+
+my_boolean = True
+print(my_boolean)
+
+        # TIP---> Rules for variables:
+        #           1. Variable names cannot have spaces
+        #           2. Variable names may contain letters, numbers, or the underscore symbol
+        #           3. You cannot start a variable name with a number
+        #           4. You can start a variable with an underscore BUT it has a special meaning (covered later in the course)
+        #           5. You cannot use python keywords for variable names
+        #               *List of keywords here: http://theselftaughtprogrammer.io/keywords
+        #       > There are two types of errors: syntax and exception (non-synatax)
+        #               - An example of an exception error is ZeroDivisionError (Occurs if you try to divide by zero)
+        #               - Unlike syntax errors, exception errors are NOT necessarily fatal (the program may still be able to run)
+        #               - Another common error is IndentationError
+
+
+#-------------------------------------------
+# EXAMPLE: pg 
+#-------------------------------------------
+
+#-------------------------------------------
+# EXAMPLE: pg 
+#-------------------------------------------
+
+#-------------------------------------------
+# EXAMPLE: pg 
+#-------------------------------------------
