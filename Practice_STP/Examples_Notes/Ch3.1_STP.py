e470dd3ea9be4e8d487c98f5e542b5ac079764f0
hashtagallison <allisonwykle@gmail.com>
2019-04-06 22:12:32 -0500

    renamed for clarity

--- Practice_STP/Examples_Notes/Ch3.1_STP.py
+++ Practice_STP/Examples_Notes/Ch3.1_STP.py
@@ -1,6 +1,6 @@
 # https://www.theselftaughtprogrammer.io
 # Cory Althoff - The Self-taught Programmer
-# Chapter 3 pg 13 - Intro to Programming
+# Chapter 3.1 pg 13 - Intro to Programming
 # hashtagallison - Practice 2019-04-04
 #-------------------------------------------
 
---
+++ Practice_STP/Examples_Notes/Ch5.2_STP.py
@@ -0,0 +1,14 @@
+# https://www.theselftaughtprogrammer.io
+# Cory Althoff - The Self-taught Programmer
+# Chapter 5.2 pg 73 - Containers - Tuples
+# hashtagallison - Practice 2019-04-06
+#-------------------------------------------
+
+
+        # TIP---> 
+
+#-------------------------------------------
+# EXAMPLE: pg 
+#-------------------------------------------
+
+        # TIP---> 
