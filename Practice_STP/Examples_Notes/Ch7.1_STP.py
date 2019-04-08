8b56748778b107216562f11e3a269220882c3503
hashtagallison <allisonwykle@gmail.com>
2019-04-08 18:25:16 -0500

    Updated template

---
+++ Practice_STP/Examples_Notes/Ch7.1_STP.py
@@ -0,0 +1,15 @@
+# https://www.theselftaughtprogrammer.io
+# Cory Althoff - The Self-taught Programmer
+# Chapter 7.1 pg 103 - Loops
+# hashtagallison - Practice 2019-04-08
+#-------------------------------------------
+
+
+        # TIP---> 
+
+#-------------------------------------------
+# EXAMPLE: pg 103
+#-------------------------------------------
+
+        # TIP---> 
+ 
