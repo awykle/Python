0fb5088c8926e2a53ba3942ff1fa75668721fb68
hashtagallison <allisonwykle@gmail.com>
2019-04-09 08:01:45 -0500

    Added more challenges!

--- Practice_STP/Challenge_Questions/Ch7_Challenge1.py
+++ Practice_STP/Challenge_Questions/Ch7_Challenge1.py
@@ -12,15 +12,29 @@
 
 #1. Print each item in the following list: ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"].
 
+hbo = [
+    "The Walking Dead", 
+    "Entourage", 
+    "The Sopranos", 
+    "The Vampire Diaries"
+    ]
+
+for recommend in hbo:
+    print(recommend)
 
 
 #2. Print all the numbers from 25 to 50.
 
+for i in range(25,51):
+    print(i)
 
 
 #3. Print each item in the list from the first challenge and their indexes.
 
-
+i = 0
+for recommend in hbo:
+    print(str(i) + " - " + recommend)
+    i += 1
 
 #4. Write a program with an infinite loop (with the option to type q to quit) and a list of numbers. Each time through
 #   the loop, ask the user to guess a number on the list and tell them whether or not they guessed correctly.
