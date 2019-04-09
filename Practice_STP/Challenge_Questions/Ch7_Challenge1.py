49bb36af18727b8fd3002fce6934fb211c9ab884
hashtagallison <allisonwykle@gmail.com>
2019-04-08 22:34:11 -0500

    Added Ch 7 questions

---
+++ Practice_STP/Challenge_Questions/Ch7_Challenge1.py
@@ -0,0 +1,31 @@
+# https://www.theselftaughtprogrammer.io
+# Cory Althoff - The Self-taught Programmer
+# Chapter 7 pg 114 - Challenges
+# hashtagallison - Practice 2019-04-08
+#-------------------------------------------
+
+#-------------------------------------------
+#       CHAPTER 7: CHALLENGES
+#               - Official Answer Key: http://tinyurl.com/z2m2ll5  (Don't look until you try for yourself!!)
+#-------------------------------------------
+
+
+#1. Print each item in the following list: ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"].
+
+
+
+#2. Print all the numbers from 25 to 50.
+
+
+
+#3. Print each item in the list from the first challenge and their indexes.
+
+
+
+#4. Write a program with an infinite loop (with the option to type q to quit) and a list of numbers. Each time through
+#   the loop, ask the user to guess a number on the list and tell them whether or not they guessed correctly.
+
+
+
+#5. Multiply all the numbers in the list [8, 19, 148, 4] will all the numbers in the list [9, 1, 33, 83], and append
+#   each result to a third list.
